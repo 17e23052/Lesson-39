caf678b5b1700383c3ed55dcde15ea775125f3f3
Mathman <70949660+17e23052@users.noreply.github.com>
2021-09-27 11:02:57 +0000

    scores.txt

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-file = open("calculation.txt", "w")
-print("Would you like to 1. add, 2. divide, 3. multiply or 4. subtract? Enter 1 to 4")
-operation = int(input())
-print("Enter a number:")
-num1 = int(input())
-print("Enter another number:")
-num2 = int(input())
-if operation == 1:
-  answer = num1 + num2
-elif operation == 2:
-  answer = num1 / num2
-elif operation == 3:
-  answer = num1 * num2
-elif operation == 4:
-  answer == num1 - num2
-else:
-  print("Invalid input")
-file.write(str(answer))
+import datetime
+file = open("scores.txt", "a")
+print("Enter your latest score:")
+score = int(input())
+currenttime = datetime.datetime.now()
+timestamp = currenttime.strftime("%d-%m-%Y %H:%M:%S")
+newscore = (f"\nScore of {score} recorded at {currenttime}")
+file.write(newscore)
 file.close()
