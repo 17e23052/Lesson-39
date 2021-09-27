fe6334a462e6d3dafb0c04b97507833b391e6bd0
Mathman <70949660+17e23052@users.noreply.github.com>
2021-09-27 11:13:05 +0000

    rnj.txt

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import datetime
-file = open("scores.txt", "a")
-print("Enter your latest score:")
-score = int(input())
-currenttime = datetime.datetime.now()
-timestamp = currenttime.strftime("%d-%m-%Y %H:%M:%S")
-newscore = (f"\nScore of {score} recorded at {currenttime}")
-file.write(newscore)
+file = open("rnj.txt", "r")
+a = 0
+for line in file:
+  for letter in line:
+    if letter == "a":
+      a = a + 1
+file.close()
+file = open("rnj.txt", "a")
+file.write(f"\n\nLetter: a\nOccurences: {a}")
 file.close()
